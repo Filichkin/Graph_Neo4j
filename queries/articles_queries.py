525ca0be816b55d21632de92157b5a336aaaada6
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-07-08 10:36:37 +0300

    added articles_queries

---
+++ queries/articles_queries.py
@@ -0,0 +1,22 @@
+query_category = '''
+LOAD CSV WITH HEADERS FROM
+'file:///Category.csv'
+AS line FIELDTERMINATOR ','
+MERGE (category:Category {categoryID: line.title})
+  ON CREATE SET category.title = line.title;
+'''
+
+query_articles = '''
+LOAD CSV WITH HEADERS FROM
+'file:///Articles.csv'
+AS line FIELDTERMINATOR ','
+MERGE (article:Article {articleID: line.title})
+'''
+
+query_reader = '''
+LOAD CSV WITH HEADERS FROM
+'file:///Reader.csv'
+AS line FIELDTERMINATOR ','
+MERGE (reader:Reader {readerID: line.name})
+  ON CREATE SET reader.nickname = line.nickname,reader.email = line.email;
+'''
