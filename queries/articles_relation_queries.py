b4b57bb6bfd09e89f7ee699801b960068086668a
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-07-08 11:39:43 +0300

    articles_relation_queries.py

---
+++ queries/articles_relation_queries.py
@@ -0,0 +1,17 @@
+query_category_articles = '''
+LOAD CSV WITH HEADERS FROM
+'file:///Category_articles.csv'
+AS line
+MATCH (category:Category {categoryID: line.title_category})
+MATCH (article:Article {articleID: line.title_article})
+CREATE (article)-[:IS_IN]->(category);
+'''
+
+query_read_articles = '''
+LOAD CSV WITH HEADERS FROM
+'file:///read_articles.csv'
+AS line
+MATCH (reader:Reader {readerID: line.name})
+MATCH (article:Article {articleID: line.title_article})
+CREATE (reader)-[:READ]->(article);
+'''
